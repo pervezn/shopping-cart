48956f309b363706c086f6ce043ec2c4daaf1929
pervezn <nidapervez2020@u.northwestern.edu>
2019-10-07 15:55:51 -0500

    photos and grid are made

--- src/App.js
+++ src/App.js
@@ -1,44 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import 'rbx/index.css';
-import { Button, Container, Title, Card, Column, Tile, Level, Image, Media, Navbar } from 'rbx';
+import AppBar from '@material-ui/core/Button';
+import { Button, Container, Title, Card, Column, Level, Image, Media, Navbar, Dropdown, Icon } from 'rbx';
+// import { fas } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+// let picturesLarge = ["100_1", "101_1","876661122392077_1", "5619496040738316_1", "6090484789343891_1", "8552515751438644_1", "9197907543445676_1", "10412368723880252_1", "10547961582846888_1", "10686354557628304_1","11033926921508488_1","11600983276356164_1"]
 const ProductTile = ({img, title, price}) => ( //creates one tile
         <Card width="300px">
           <Card.Image>
             <Image.Container>
               <Image src= { img } />
+              {/* {console.log("img is: ", {img})} */}
             </Image.Container>
           </Card.Image>
           <Card.Content>
             <Media.Item>
-              <Title>{ title }</Title>
-              <Title>{ price }</Title>
+             <Level.Item><Title subtitle={true}>{ title }</Title></Level.Item>
+              <Level.Item><Title subtitle={true}>{ price }</Title></Level.Item>
             </Media.Item>
           </Card.Content>
           <Level.Item>
-            <Button>Add to Cart</Button>
+            <Button size={"medium"} color={"black"} fullwidth={true}>Add to Cart</Button>
           </Level.Item>
         </Card>
 );
 
 
 const ProductTable = ({productList}) => (
-  <Level>
-    <Level.Item>
-      {/* <Tile size={20}> */}
-      <Column.Group multiline={ true }>
-        { productList.map(product => 
-          <Column key={ product.sku}>  {/* change key */}
-            <ProductTile key={ product.sku } 
-                        img={ product.img } 
+  <Column.Group multiline={ true }>
+    { productList.map(product =>
+      {
+        let str = "./data/products/" + product.sku + "_1.jpg";
+        return (
+        <Column key={ product.sku} size={3}>  {/* change key */}
+          <ProductTile key={ product.sku } 
+                      img={ str }
                         title={ product.title }
-                        price={ product.price } />
-          </Column> )}
-      </Column.Group>
-      {/* </Tile> */}
-    </Level.Item>
-  </Level>
+                        price={ product.currencyFormat + product.price } />
+         </Column>); 
+      }
+    )
+    }
+  </Column.Group>
 );
+
+
+
+const PriceFilter = ({}) => (
+  <Dropdown>
+            <Dropdown.Trigger>
+              <Button>
+               <span>Select</span>
+               <Icon>
+                 {/* <FontAwesomeIcon icon={fas}/> */}
+                 {/* <i class="fas fa-caret-down"></i> */}
+               </Icon>
+             </Button>
+            </Dropdown.Trigger>
+              <Dropdown.Menu>
+                <Dropdown.Content>
+                  <Dropdown.Item>Select</Dropdown.Item>
+                  <Dropdown.Item>Lowest to Highest</Dropdown.Item>
+                  <Dropdown.Item>Highest to Lowest</Dropdown.Item>
+                </Dropdown.Content>
+              </Dropdown.Menu>
+          </Dropdown>
+);
+
+
 
 const App = () => {
   const [data, setData] = useState({});
@@ -54,12 +84,37 @@ const App = () => {
 
   return (
     <Container>
-      {/* <Navbar.Menu>Menu</Navbar.Menu> */}
-      <ProductTable productList={ products} />
+      <Navbar transparent>
+        <Navbar.Segment align="end">
+          <Navbar.Item>
+              <Title>shopping cart</Title>
+          </Navbar.Item> 
+        </Navbar.Segment>
+      </Navbar>
+      <Column.Group>
+        <Column size="one-fifth" multiline={true}>
+          <Title size={5}>Sizes:</Title>
+          <Button.Group >
+            <Button rounded={true} color={"light"}>S</Button>
+            <Button rounded={true} color={"light"}>M</Button>
+            <Button rounded={true} color={"light"}>L</Button>
+            <Button rounded={true} color={"light"}>XL</Button>
+          </Button.Group>
+        </Column>
+        <Column>
+          <Container>
+            <Level>
+              <Level.Item align="Left">X Product(s) found. </Level.Item>
+              <Level.Item as="div" align="right">
+                <Title subtitle={true} spaced={true}> Order By  </Title> 
+                <PriceFilter />
+              </Level.Item>
+            </Level>
+            <ProductTable productList={products} />
+          </Container>
+        </Column>
+      </Column.Group>
     </Container>
-    // <ul>
-    //   {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    // </ul>
   );
 };
 
