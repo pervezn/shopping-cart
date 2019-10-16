ddc0517be8aee94c512e60c22021dc31f6d0faf1
pervezn <nidapervez2020@u.northwestern.edu>
2019-10-15 21:06:35 -0500

    worked on logic for implementing cart

--- src/App.js
+++ src/App.js
@@ -5,26 +5,41 @@ import { Button, Container, Title, Card, Column, Level, Image, Media, Navbar, Dr
 // import { fas } from "@fortawesome/free-solid-svg-icons";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
-
-const useViewSidebar = () => {
-  const [visible, setVisibility] = useState(false);
-  const toggle = () => {
-      setVisibility(visible ? /*show Sidebar*/true : /*hide Sidebar*/false)
-      //show and not show sidebar?
-  };
-  return [visible, toggle];
-}
-const useSidebar = () => {
-  const [sidebarItems, setSidebarItems] = useState([]);
-  const editItemsList = (newItem) => {
-    setSidebarItems(sidebarItems.push(newItem)) //adds Item
-    //add, remove item from list?
-  };
-  return [sidebarItems, editItemsList];
+const buttonStyle ={
+  margin: "15px"
 }
 
-const ProductTile = ({img, title, price}) => ( //creates one tile
+const cartStyle = {
+  sidebar: {
+    // padding: "5px",
+    position: "relative",
+    background: "black"
+  },
+  overlay: {
+    position:"absolute"
+  }
+}
+
+const CartTile = ({img, title, size, desc, quantity, price}) => {
+  <Level>
+    <Card>
+      <Card.Image>
+        <Image.Container>
+          <Image src={ img }/>
+        </Image.Container>
+      </Card.Image>
+    <Title> {title} </Title>
+    <Title subtitle>{ size }</Title><Title subtitle>{ desc }</Title>
+    <Title subtitle>Quantity: { quantity }</Title>
+    <Title subtitle>{price}</Title>
+    </Card>
+  </Level>
+}
+
+const ProductTile = ({img, title, price}) => { //creates one tile
+  const [cartList, setCartList] = useState([]);
+
+  return (
         <Card width="300px">
           <Card.Image>
             <Image.Container>
@@ -39,59 +54,119 @@ const ProductTile = ({img, title, price}) => ( //creates one tile
             </Media.Item>
           </Card.Content>
           <Level.Item>
-            <Button size={"medium"} color={"black"} fullwidth={true}>Add to Cart</Button>
+            <Button size={"medium"} color={"black"} fullwidth={true} onClick={() => {
+              addToCart(title, cartList); 
+              setCartList(cartList)}
+            }>Add to Cart</Button>  
           </Level.Item>
         </Card>
+  );
+};
+
+function addToCart(title, cartList) {
+  let contains = false
+  let index;
+  for(let i = 0; i < cartList.length -1; i++){
+    if(cartList[i][0] === title) {
+      contains = true;
+      index = i;
+    }
+  }
+  if(contains){
+    cartList[index][1]++
+  } else {
+    cartList.push([title, 1]);
+  }
+  return cartList
+}
+
+const CartContainer = ({ cartOpen, setCartOpen }) => (
+  <Sidebar sidebar={"Hello"}
+           pullRight={true}
+           open={cartOpen}
+           onSetOpen={setCartOpen}
+           styles={cartStyle}>
+    <button style={buttonStyle} onClick={() => {
+      setCartOpen(true);
+      cartList.map(item =>  //HOW DO I PASS THE CARTLIST TO HERE?????
+        <CartTile >
+          img= {item.img}
+          title={item.title}
+          size={item.size}
+          desc={item.desc}
+          quantity={item.quantity}
+          price={item.price}
+        </CartTile>)
+    }
+    }>Cart</button>
+  </Sidebar>
 );
 
-
 const ProductTable = ({productList}) => (
-  <Column.Group multiline={ true }>
+  <Column.Group multiline>
     { productList.map(product =>
-      {
-        let str = "./data/products/" + product.sku + "_1.jpg";
-        return (
-        <Column key={ product.sku} size={4}>  {/* change key */}
-          <ProductTile key={ product.sku } 
-                      img={ str }
-                        title={ product.title }
-                        price={ product.currencyFormat + product.price } />
-         </Column>); 
-      }
-    )
+        {
+          let str = "./data/products/" + product.sku + "_1.jpg";
+          return (
+          <Column key={ product.sku} size={4}>  {/* change key */}
+            <ProductTile key={ product.sku } 
+                        img={ str }
+                          title={ product.title }
+                          price={ product.currencyFormat + product.price } />
+          </Column>); 
+        }
+      )
     }
   </Column.Group>
 );
 
-
-
 const PriceFilter = ({}) => (
   <Dropdown>
-            <Dropdown.Trigger>
-              <Button>
-               <span>Select</span>
-               <Icon>
-                 {/* <FontAwesomeIcon icon={fas}/> */}
-                 {/* <i class="fas fa-caret-down"></i> */}
-               </Icon>
-             </Button>
-            </Dropdown.Trigger>
-              <Dropdown.Menu>
-                <Dropdown.Content>
-                  <Dropdown.Item>Select</Dropdown.Item>
-                  <Dropdown.Item>Lowest to Highest</Dropdown.Item>
-                  <Dropdown.Item>Highest to Lowest</Dropdown.Item>
-                </Dropdown.Content>
-              </Dropdown.Menu>
-          </Dropdown>
+    <Dropdown.Trigger>
+      <Button>
+        <span>Select</span>
+        <Icon>
+          {/* <FontAwesomeIcon icon={fas}/> */}
+          {/* <i class="fas fa-caret-down"></i> */}
+        </Icon>
+      </Button>
+    </Dropdown.Trigger>
+      <Dropdown.Menu>
+        <Dropdown.Content>
+          <Dropdown.Item>Select</Dropdown.Item>
+          <Dropdown.Item>Lowest to Highest</Dropdown.Item>
+          <Dropdown.Item>Highest to Lowest</Dropdown.Item>
+        </Dropdown.Content>
+      </Dropdown.Menu>
+  </Dropdown>
 );
 
+const SizeFilter = ({}) => (
+  <Column size="one-fifth" >
+    <Title size={5}>Sizes:</Title>
+    <Button.Group >
+      <Button rounded={true} color={"light"}>S</Button>
+      <Button rounded={true} color={"light"}>M</Button>
+      <Button rounded={true} color={"light"}>L</Button>
+      <Button rounded={true} color={"light"}>XL</Button>
+    </Button.Group>
+  </Column>
+)
 
+const PriceFilterRow = ({}) => (
+  <Level>
+    <Level.Item align="Left">X Product(s) found. </Level.Item>
+    <Level.Item as="div" align="right">
+      <Title subtitle={true} spaced={true}> Order By  </Title> 
+      <PriceFilter />
+    </Level.Item>
+  </Level>
+)
 
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
-  const [visible, toggle] = useViewSidebar();
+  const [cartOpen, setCartOpen] = useState(false);
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
@@ -103,32 +178,12 @@ const App = () => {
 
   return (
     <Container>
-      <Navbar transparent>
-        <Navbar.Segment align="end">
-          <Navbar.Item>
-              <Sidebar>Shopping Cart</Sidebar>
-          </Navbar.Item> 
-        </Navbar.Segment>
-      </Navbar>
       <Column.Group>
-        <Column size="one-fifth" multiline={true}>
-          <Title size={5}>Sizes:</Title>
-          <Button.Group >
-            <Button rounded={true} color={"light"}>S</Button>
-            <Button rounded={true} color={"light"}>M</Button>
-            <Button rounded={true} color={"light"}>L</Button>
-            <Button rounded={true} color={"light"}>XL</Button>
-          </Button.Group>
-        </Column>
+        <Column size={1}><CartContainer cartOpen={cartOpen} setCartOpen={setCartOpen}/></Column>
+        <SizeFilter/>
         <Column>
           <Container>
-            <Level>
-              <Level.Item align="Left">X Product(s) found. </Level.Item>
-              <Level.Item as="div" align="right">
-                <Title subtitle={true} spaced={true}> Order By  </Title> 
-                <PriceFilter />
-              </Level.Item>
-            </Level>
+            <PriceFilterRow/>
             <ProductTable productList={products} />
           </Container>
         </Column>
